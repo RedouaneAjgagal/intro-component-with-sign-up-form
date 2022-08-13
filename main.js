26f7c014c359cb05cc7e28c1ffc0ab38f46f830f
RedouaneAjgagal <98456832+RedouaneAjgagal@users.noreply.github.com>
2022-08-13 23:13:52 +0200

    Minify the code

--- main.js
+++ main.js
@@ -25,67 +25,49 @@ form.addEventListener("submit", (e) => {
 // First name
 function checkFirstName(arr, req) {
     if (firstName.value === '' || firstName.value === null) {
-        addBorderErr(firstName);
-        addErrIcon(firstName);
-        errText[0].innerHTML = 'Cannot be empty';
-        arr.push(errText[0].innerHTML);
+        addErr(firstName);
+        arr.push(errText[0].innerHTML = 'Cannot be empty');
     } else if (!req.test(firstName.value)) {
-        addBorderErr(firstName);
-        addErrIcon(firstName);
-        errText[0].innerHTML = 'At least 3 Characters';
-        arr.push(errText[0].innerHTML);
+        addErr(firstName);
+        arr.push(errText[0].innerHTML = 'At least 3 Characters');
     } else {
-        removeBorderErr(firstName);
-        removeErrIcon(firstName);
+        removeErr(firstName);
         errText[0].innerHTML = '';
     }
 }
 // Last name
 function checkLastName(arr, req) {
     if (lastName.value === '' || lastName.value === null) {
-        addBorderErr(lastName);
-        addErrIcon(lastName);
-        errText[1].innerHTML = 'Cannot be Empty';
-        arr.push(errText[1].innerHTML);
+        addErr(lastName);
+        arr.push(errText[1].innerHTML = 'Cannot be Empty');
     } else if (!req.test(lastName.value)) {
-        addBorderErr(lastName);
-        addErrIcon(lastName);
-        errText[1].innerHTML = 'At least 3 Characters';
-        arr.push(errText[1].innerHTML);
+        addErr(lastName);
+        arr.push(errText[1].innerHTML = 'At least 3 Characters');
     } else {
-        removeBorderErr(lastName);
-        removeErrIcon(lastName);
+        removeErr(lastName);
         errText[1].innerHTML = '';
     }
 }
 // Email
 function checkEmail(arr, req) {
     if (email.value === '' || email.value === null) {
-        addBorderErr(email);
-        addErrIcon(email);
-        errText[2].innerHTML = 'Cannot be Empty';
-        arr.push(errText[2].innerHTML);
+        addErr(email);
+        arr.push(errText[2].innerHTML = 'Cannot be Empty');
     } else if (!req.test(email.value)) {
-        addBorderErr(email);
-        addErrIcon(email);
-        errText[2].innerHTML = 'It looks like invalide Email';
-        arr.push(errText[2].innerHTML);
+        addErr(email);
+        arr.push(errText[2].innerHTML = 'It looks like invalide Email');
     } else {
-        removeBorderErr(email);
-        removeErrIcon(email);
+        removeErr(email);
         errText[2].innerHTML = '';
     }
 }
 // Password
 function checkPassword(arr, req) {
     if (password.value === '' || password.value === null) {
-        addBorderErr(password);
-        addErrIcon(password);
-        errText[3].innerHTML = 'Cannot be Empty';
-        arr.push(errText[3].innerHTML);
+        addErr(password);
+        arr.push(errText[3].innerHTML = 'Cannot be Empty');
     } else if (!req.test(password.value)) {
-        addBorderErr(password);
-        addErrIcon(password);
+        addErr(password);
         if (password.value.length > 20) {
             errText[3].innerHTML = 'Password is too Long';
         } else {
@@ -93,26 +75,18 @@ function checkPassword(arr, req) {
         }
         arr.push(errText[3].innerHTML);
     } else {
-        removeBorderErr(password);
-        removeErrIcon(password);
+        removeErr(password);
         errText[3].innerHTML = '';
     }
 }
 
-// Add error icon
-function addErrIcon(elm) {
+// Add error
+function addErr(elm) {
     elm.nextElementSibling.setAttribute("src", "./images/icon-error.svg");
-}
-// Remove error icon
-function removeErrIcon(elm) {
-    elm.nextElementSibling.removeAttribute("src");
-}
-
-// Add Border error 
-function addBorderErr(elm) {
     elm.setAttribute("class", "errBorder");
 }
-// Remove Border error
-function removeBorderErr(elm) {
+// Remove error
+function removeErr(elm) {
+    elm.nextElementSibling.removeAttribute("src");
     elm.removeAttribute("class");
 }
